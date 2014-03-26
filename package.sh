d4294f7cb0073e157fba1b21f52eafd15a6b758c
objectundefined <gabriel.lipson@gmail.com>
2014-03-26 13:45:46 -0700

    pkg script

--- package.sh
+++ package.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 rm -rf target/
-MAVEN_OPTS="-Xmx256M -Xms256M -server -d64" mvn compile package
+mvn clean
+MAVEN_OPTS="-Xmx256M -Xms256M -server -d64" mvn compile
+MAVEN_OPTS="-Xmx256M -Xms256M -server -d64" mvn package
