6b467e6aa647583e4b20ad27e6689f2499ab2a47
objectundefined <gabriel.lipson@gmail.com>
2014-03-26 02:56:14 -0700

    mvn cleanup

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rm -rf target/
-MAVEN_OPTS="-Xmx256M -Xms256M -server -d64" mvn clean compile package -Djava.library.path=/usr/local/lib -Dexec.mainClass=org.neozmq.ZmqKernelExtensionFactory
+MAVEN_OPTS="-Xmx256M -Xms256M -server -d64" mvn compile package
